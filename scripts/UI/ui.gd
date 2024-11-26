67d786d705df73d0d3e02f1b61bdf2b372c34a69
aprianwdh <aprianwidhiwibowo@gmail.com>
2024-11-26 19:07:37 +0700

    membuat animasi menembak

--- scripts/UI/ui.gd
+++ scripts/UI/ui.gd
@@ -1,22 +1,31 @@
 extends CanvasLayer
 
-var current_wepon = 'knife'
-var ammo = 0
+var current_wepon = Global.current_weapon
+var ammo = Global.ammo
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	current_wepon = 'gun'
+	ammo = 5
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+		
 	if Input.is_action_just_pressed('ui_accept'):
 		if current_wepon == 'knife':
 			$AnimatedSprite2D.play("tusuk")
 		elif current_wepon == 'gun':
 			if ammo > 0:
 				$AnimatedSprite2D.play('shoot')
-				ammo -= 0
+				ammo -= 1
+			else :
+				current_wepon = 'knife'
+				$AnimatedSprite2D.play("iddle_knife")
+	print(ammo)
 
 
 func _on_animated_sprite_2d_animation_finished():
---
+++ scripts/core/global.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var ammo : int = 0
+var current_weapon : String
+
+
